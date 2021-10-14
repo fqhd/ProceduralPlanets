280becaba4ad9bb6f3d00e8b7b7e561226aca6d9
fqhd <fahd31256@gmail.com>
2021-10-14 22:33:18 +0100

    loadShader()

--- js/main.js
+++ js/main.js
@@ -13,6 +13,30 @@ function main(){
 
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
+
+
+	const fsSource = "void main(){gl_FragColor = vec4(1, 0, 0, 1);}";
+
+	loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+}
+
+function loadShader(gl, type, source){
+	// Creating the shader object
+	const shader = gl.createShader(type);
+
+	// Sending the source to the shader object
+	gl.shaderSource(shader, source);
+
+	// Compiling the shader
+	gl.compileShader(shader);
+
+	// Checking if the shader has been compiled properly
+	if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
+		alert('Failed to compile shader!');
+		return null;
+	}
+
+	return shader;
 }
 
 window.onload = main;
