788106ee47e19b2d325063a428ccae99b3aba7cd
fqhd <fahd31256@gmail.com>
2021-10-26 20:33:30 +0100

    test: add cube entity

--- js/application.js
+++ js/application.js
@@ -18,7 +18,11 @@ export async function init(gl){
 		entities: [
 			{
 				model: models.bunny,
-				transform: init_transform([0, 0, -5], [35, 0, 0], [1, 1 ,1]),
+				transform: init_transform([2, 0, -10], [35, 0, 0], [1, 1 ,1]),
+			},
+			{
+				model: models.cube,
+				transform: init_transform([-2, 0, -10], [35, 0, 0], [1, 1 ,1]),
 			},
 		],
 		camera: create_camera([0, 0, 0], 0, 0, gl.canvas.clientWidth / gl.canvas.clientHeight),
--- js/engine/model_global.js
+++ js/engine/model_global.js
@@ -5,5 +5,6 @@ import {load_model_from_file} from '/js/engine/model.js';
 export async function load_models(gl){
 	return {
 		bunny: await load_model_from_file(gl, 'res/models/normal_bunny.obj'),
+		cube: await load_model_from_file(gl, 'res/models/cube.obj'),
 	};
 }
