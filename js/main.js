60a44bed57382c68cdcebef0393d2e513a8f6e71
fqhd <fahd31256@gmail.com>
2021-11-03 16:13:58 +0100

    fix: canvas size

--- js/main.js
+++ js/main.js
@@ -9,16 +9,13 @@ async function main(){
 	const canvas = document.getElementById('canvas');
 
 	gl = canvas.getContext('webgl2');
+
 	if(gl == null){
 		console.error('Failed to initialize webgl');
 		return;
 	}
 	console.log('WebGL Version: ' + gl.getParameter(gl.VERSION));
 	console.log('GLSL Version: ' + gl.getParameter(gl.SHADING_LANGUAGE_VERSION));
-
-	// Updating the canvas size
-	canvas.width = gl.canvas.clientWidth;
-	canvas.height = gl.canvas.clientHeight;
 
 	// Initialization of the mythical super data structure
 	scene_data = await init(gl);
