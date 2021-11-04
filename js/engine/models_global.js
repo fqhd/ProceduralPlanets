f862c82771e55f8405179a5efc58a404ee55d19e
fqhd <fahd31256@gmail.com>
2021-11-04 19:47:13 +0100

    fix: remove monkey and cube models

--- js/engine/models_global.js
+++ js/engine/models_global.js
@@ -5,7 +5,5 @@ import {load_model_from_file} from './model.js';
 export async function load_models(gl){
 	return {
 		bunny: await load_model_from_file(gl, 'res/models/bunny.obj'),
-		cube: await load_model_from_file(gl, 'res/models/cube.obj'),
-		monkey: await load_model_from_file(gl, 'res/models/monkey.obj'),
 	};
 }
--- js/engine/textures_global.js
+++ js/engine/textures_global.js
@@ -4,6 +4,7 @@ import {load_texture_from_file} from './texture.js';
 
 export async function load_textures(gl){
     return {
-        bricks: await load_texture_from_file(gl, '/res/textures/bricks.jpg'),
+        bricks_texture: await load_texture_from_file(gl, '/res/textures/bricks_texture.jpg'),
+        bricks_normal: await load_texture_from_file(gl, '/res/textures/bricks_normal.jpg'),
     };
 }
