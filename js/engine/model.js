7b405be5fdf4aa2c0dbf1a7671a766eb9a0d1c55
fqhd <fahd31256@gmail.com>
2021-11-04 19:34:40 +0100

    fix: proper tangent vectors

--- js/engine/model.js
+++ js/engine/model.js
@@ -75,17 +75,20 @@ function mul_vec3(a, b){
 }
 
 function calc_tangents(positions, uvs){
-	let tangents = [];
+	const tangents = [];
+
+	let j = 0;
 
 	for(let i = 0; i < positions.length; i+=9){
 		const P0 = [positions[i], positions[i+1], positions[i+2]];
 		const P1 = [positions[i+3], positions[i+4], positions[i+5]];
 		const P2 = [positions[i+6], positions[i+7], positions[i+8]];
 
-		const T0 = [uvs[i], uvs[i+1]];
-		const T1 = [uvs[i+2], uvs[i+3]];
-		const T2 = [uvs[i+4], uvs[i+5]];
-
+		const T0 = [uvs[j], uvs[j+1]];
+		const T1 = [uvs[j+2], uvs[j+3]];
+		const T2 = [uvs[j+4], uvs[j+5]];
+		j+=6;
+		
 		const delta_pos_1 = sub_vec3(P1, P0);
 		const delta_pos_2 = sub_vec3(P2, P0);
 
