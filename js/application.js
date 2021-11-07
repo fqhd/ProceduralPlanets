6ade587f2acec4315e60fa0eba391a81ef080101
fqhd <fahd31256@gmail.com>
2021-11-07 10:42:30 +0100

    refactor: remove unnecessary models_global.js

--- js/application.js
+++ js/application.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import {load_shaders, bind_shader_tex_attribs} from '/js/engine/shader.js';
-import {load_models} from '/js/engine/models_global.js';
+import {load_models} from '/js/engine/model.js';
 import {load_textures} from '/js/engine/texture.js';
 import {draw_scene, init_gl_state} from '/js/engine/renderer.js';
 import {create_camera} from '/js/engine/camera.js';
--- js/engine/model.js
+++ js/engine/model.js
@@ -2,7 +2,14 @@
 
 const {vec3} = glMatrix;
 
-export async function load_model_from_file(gl, path){
+export async function load_models(gl){
+	return {
+		bunny: await load_model_from_file(gl, 'res/models/bunny.obj'),
+		plane: await load_model_from_file(gl, 'res/models/plane.obj'),
+	};
+}
+
+async function load_model_from_file(gl, path){
 	const response = await fetch(path);
 	const text = await response.text();
 	const lines = text.split('\n');
