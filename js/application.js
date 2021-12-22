2419ad71972acce2d13d757d328d90db6c8127a7
fqhd <fahd31256@gmail.com>
2021-12-22 12:56:12 +0100

    feat: connect texture sliders to javascript

--- js/application.js
+++ js/application.js
@@ -7,6 +7,10 @@ import { create_moon_model } from './engine/moon_generator.js';
 import { load_textures } from './engine/texture.js';
 import { get_random_color } from './engine/utils.js';
 
+let nmap_strength_slider;
+let nmap_scale_slider;
+let nmap_sharpness_slider;
+
 export async function init_application(gl){
 	noise.seed(Math.random());
 	
@@ -16,16 +20,14 @@ export async function init_application(gl){
 		load_textures(gl),
 	]);
 
-	const moon_params = get_moon_params();
-
 	const scene = {
 		shaders,
 		skybox,
 		moon: {
-			model: create_moon_model(gl, moon_params),
+			model: create_moon_model(gl),
 			normal_map_1: textures.normal_map_1,
 			normal_map_2: textures.normal_map_2,
-			params: moon_params,
+			params: get_moon_params(),
 		},
 		camera: create_camera(40, 0, gl.canvas.clientWidth / gl.canvas.clientHeight),
 	};
@@ -34,11 +36,21 @@ export async function init_application(gl){
 	init_gl_state(gl);
 	bind_shader_uniforms(gl, shaders);
 	init_controls(scene);
+	init_sliders();
 	return scene;
 }
 
+function init_sliders(){
+	nmap_strength_slider = document.getElementById('nmap-strength');
+	nmap_scale_slider = document.getElementById('nmap-scale');
+	nmap_sharpness_slider = document.getElementById('nmap-sharpness');
+}
+
 export function update(gl, scene){
-	
+	const moon_params = scene.moon.params;
+	moon_params.texture_scale = (nmap_scale_slider.value / 100) * 40;
+	moon_params.blend_sharpness = (nmap_sharpness_slider.value / 100) * 50;
+	moon_params.nmap_strength = nmap_strength_slider.value / 100;
 }
 
 export function draw(gl, scene){
