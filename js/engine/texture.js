c00efa54258f9e760353b687c398e8acb0ea160f
fqhd <fahd31256@gmail.com>
2021-12-30 16:54:25 +0100

    refactor: remove additional unnecessary functions

--- js/engine/texture.js
+++ js/engine/texture.js
@@ -45,26 +45,20 @@ export function create_indices_texture(gl, indices){
 }
 
 export async function load_texture_from_file(gl, path_to_file){
-	const texture = gl.createTexture();
-	gl.bindTexture(gl.TEXTURE_2D, texture);
-
 	const image = await load_image(path_to_file);
-
-	gl.bindTexture(gl.TEXTURE_2D, texture);
+	
+	const id = gl.createTexture();
+	gl.bindTexture(gl.TEXTURE_2D, id);
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);
-
 	gl.generateMipmap(gl.TEXTURE_2D);
-	const ext = gl.getExtension('EXT_texture_filter_anisotropic');
-	if(ext != null){
-		const amount = Math.min(4, gl.getParameter(ext.MAX_TEXTURE_MAX_ANISOTROPY_EXT));
-		gl.texParameteri(gl.TEXTURE_2D, ext.TEXTURE_MAX_ANISOTROPY_EXT, amount);
-	}else{
-		console.log('Browser does not support anisotropic filtering extension');
-	}
 
-	return texture;
+	return {
+		id,
+		width: image.width,
+		height: image.height
+	};
 }
